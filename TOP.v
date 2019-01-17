29e1fe57d5172c41d4b417dcb4ca5a57ffa3328e
Rihards Skuja <rssk@protonmail.com>
2019-01-18 00:33:54 +0200

    Update TOP and VGA components to their latest version from master

--- TOP.v
+++ TOP.v
@@ -1,80 +1,80 @@
-`timescale 1ns / 1ps
 //////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date:    16:29:22 01/16/2019 
-// Design Name: 
-// Module Name:    TOP 
+// Company:
+// Engineer:
+//
+// Create Date:    16:29:22 01/16/2019
+// Design Name:
+// Module Name:    TOP
 // Project Name:   DIP 2018/19
-// Target Devices: 
-// Tool versions: 
+// Target Devices:
+// Tool versions:
 // Description:    VGA component for DIP 2k18
 //
-// Dependencies: 
+// Dependencies:
 //
-// Revision: 
+// Revision:
 // Revision 0.01 - File Created
-// Additional Comments: 
+// Additional Comments:
 //
 //////////////////////////////////////////////////////////////////////////////////
-
+`default_nettype none
 module TOP(
-    input CLK_50MHZ,
-    input BTN_EAST,
-    output VGA_RED,
-    output VGA_GREEN,
-    output VGA_BLUE,
-    output VGA_HSYNC,
-    output VGA_VSYNC
+    input wire CLK_50MHZ,
+
+    input wire BTN_EAST,
+
+    output wire [3:0] VGA_RED,
+    output wire [3:0] VGA_GREEN,
+    output wire [3:0] VGA_BLUE,
+
+    output wire VGA_HSYNC,
+    output wire VGA_VSYNC
+);
+
+    /*----------------------------
+        Reset button
+
+    ----------------------------
+    */
+    wire rst = ~BTN_EAST;
+
+    /*----------------------------
+        Clock divider 50MHz -> 25MHz
+
+    ----------------------------
+    */
+    reg [15:0] cnt;
+    reg stb;
+    always @(posedge CLK_50MHZ)
+        {stb, cnt} <= cnt + 16'h8000;
+
+    /*----------------------------
+        Pixel position
+
+    ----------------------------
+    */
+    wire [9:0] x;
+    wire [8:0] y;
+
+    VGA display (
+        .in_clock(CLK_50MHZ),
+        .in_strobe(stb),
+        .in_reset(rst),
+        .out_hsync(VGA_HSYNC),
+        .out_vsync(VGA_VSYNC),
+        .out_x(x),
+        .out_y(y)
     );
 
-	/*----------------------------
-		Reset button
-		
-	  ----------------------------
-	*/
-	wire rst = ~BTN_EAST;
-	
-	
-	/*----------------------------
-		Clock divider 50MHz -> 20MHz
-		
-	  ----------------------------
-	*/
-	
-	reg [15:0] cnt;
-	reg px_strobe;
-	
-	always @ (posedge CLK_50MHZ)
-		{px_strobe, cnt} <= cnt + 16'h8000;
-	
-	
-	/*----------------------------
-		Pixel position
-		
-	  ----------------------------
-	*/
-	wire [9:0] x;
-	wire [8:0] y;
-	
-	
-	VGA display 
-	(
-		.in_clock(CLK_50MHZ),
-		.in_strobe(px_strobe),
-		.in_reset(rst),
-		.out_hsync(VGA_HSYNC),
-		.out_vsync(VGA_VSYNC),
-		.out_x(x),
-		.out_y(y)
-	);
-	
-	// ps2 input goes here:
-	// wire point;
-	// assign point = x...
-	// assign point = y...
-	// 
-	// assign VGA_R[3] = point;
 
+    // Test��anas nol�kiem, �is uz ekr�na z�m� kvadr�tus. Re�li, datus, protams, dodam no bufera.
+    wire sq_a, sq_b, sq_c, sq_d;
+    assign sq_a = ((x > 120) & (y >  40) & (x < 280) & (y < 200)) ? 1 : 0;
+    assign sq_b = ((x > 200) & (y > 120) & (x < 360) & (y < 280)) ? 1 : 0;
+    assign sq_c = ((x > 280) & (y > 200) & (x < 440) & (y < 360)) ? 1 : 0;
+    assign sq_d = ((x > 360) & (y > 280) & (x < 520) & (y < 440)) ? 1 : 0;
+
+    assign VGA_RED[3] = sq_b;
+    assign VGA_GREEN[3] = sq_a | sq_d;
+    assign VGA_BLUE[3] = sq_c;
 endmodule
--- VGA.v
+++ VGA.v
@@ -1,93 +1,89 @@
-`timescale 1ns / 1ps
+`default_nettype none
 //////////////////////////////////////////////////////////////////////////////////
-// Company: 
+// Company:
 // Engineer:
-// 
-// Create Date:    15:44:46 01/16/2019 
-// Design Name: 
-// Module Name:    VGA 
-// Project Name: DIP 2018
-// Target Devices: 
-// Tool versions: 
-// Description: 
 //
-// Dependencies: 
+// Create Date:    16:29:22 01/16/2019
+// Design Name:
+// Module Name:    TOP
+// Project Name:   DIP 2018/19
+// Target Devices:
+// Tool versions:
+// Description:    VGA component for DIP 2k18
 //
-// Revision: 
+// Dependencies:
+//
+// Revision:
 // Revision 0.01 - File Created
-// Additional Comments: 
+// Additional Comments:
 //
 //////////////////////////////////////////////////////////////////////////////////
+
 module VGA(
     input wire in_clock,
     input wire in_strobe,
     input wire in_reset,
+
     output wire out_hsync,
     output wire out_vsync,
-    output wire out_blank,
-    output wire out_active,
-    output wire out_scrend,
-    output wire out_anim,
+
+    output wire out_blank,   // blanking interval
+    output wire out_active,  // visible image data/signal
+    output wire out_screen,
+
     output wire [9:0] out_x,
     output wire [8:0] out_y
-    );
-	 
-	localparam HS_STA = 16;             
-	localparam HS_END = 16 + 96;        
-	localparam HA_STA = 16 + 96 + 48;   
-	localparam VS_STA = 480 + 11;       
-	localparam VS_END = 480 + 11 + 2;   
-	localparam VA_END = 480;            
-	localparam LINE   = 800;            
-	localparam SCREEN = 524;            
+);
 
-	reg [9:0] line_pos;  // line position
-	reg [9:0] scr_pos;  // screen position
-	
-	
-	// Generate VH sync
-	assign out_hsync = ~((line_pos >= HS_STA) & (line_pos < HS_END));
-   assign out_vsync = ~((scr_pos >= VS_STA) & (scr_pos < VS_END));
-	
-	
-	// X and Y are bound to active pixels area
-	assign out_x = (line_pos < HA_STA) ? 0 : (line_pos - HA_STA);
-	assign out_y = (scr_pos >= VA_END) ? (VA_END - 1) : (scr_pos);
-	
-	
-	// Blanking Interval
-	assign out_blank = ((line_pos < HA_STA) | (scr_pos > VA_END - 1));
-	
-	// Active/Draw interval
-	assign out_active = ~((line_pos < HA_STA) | (scr_pos > VA_END - 1)); 
-	
-	// End of screen tick
-	assign out_scrend = ((scr_pos == SCREEN - 1) & (line_pos == LINE));
-	
-	// Animation tick
-	assign out_anim = ((scr_pos == VA_END - 1) & (line_pos == LINE));
+    localparam HS_STA = 16;           // Skat�t DIP VGA dokumentu
+    localparam HS_END = 16 + 96;
+    localparam HA_STA = 16 + 96 + 48;
+    localparam VS_STA = 480 + 11;
+    localparam VS_END = 480 + 11 + 2;
+    localparam VA_END = 480;
+    localparam LINE   = 800;
+    localparam SCREEN = 524;
 
-	
-	always @ (posedge i_clk)
-		begin
-        if (in_reset)  	// Reset frame
-			  begin
-					line_pos <= 0;
-					scr_pos <= 0;
-			  end
-        if (in_strobe)  // Per tick
-			  begin
-					if (line_pos == LINE)  // end of line
-						begin
-							 line_pos <= 0;
-							 scr_pos <= scr_pos + 1;
-						end
-					else 
-						 line_pos <= line_pos + 1;
+    reg [9:0] linepos;
+    reg [9:0] pixpos;
 
-					if (scr_pos == SCREEN)  // end of screen
-						 scr_pos <= 0;
-			  end
+
+    // Generate VH sync
+    assign out_hsync = ~((linepos >= HS_STA) & (linepos < HS_END));
+    assign out_vsync = ~((pixpos >= VS_STA) & (pixpos < VS_END));
+
+    // X and Y are bound to active pixels area
+    assign out_x = (linepos < HA_STA) ? 0 : (linepos - HA_STA);
+    assign out_y = (pixpos >= VA_END) ? (VA_END - 1) : (pixpos);
+
+    // Blanking Interval
+    assign out_blank = ((linepos < HA_STA) | (pixpos > VA_END - 1));
+
+    // Active/Draw interval for visible image signal
+    assign out_active = ~((linepos < HA_STA) | (pixpos > VA_END - 1));
+
+    // End of screen tick
+    assign out_screen = ((pixpos == SCREEN - 1) & (linepos == LINE));
+
+    always @ (posedge in_clock)
+    begin
+        if (in_reset)  // reset frame
+        begin
+            linepos <= 0;
+            pixpos <= 0;
+        end
+        if (in_strobe)  // per 40ns tick
+        begin
+            if (linepos == LINE)
+            begin
+                linepos <= 0;
+                pixpos <= pixpos + 1;
+            end
+            else
+                linepos <= linepos + 1;
+
+            if (pixpos == SCREEN)
+                pixpos <= 0;
+        end
     end
-
 endmodule
